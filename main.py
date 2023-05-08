d140ced325c2b1c9692b0b840126fe73ab295854
Theresia Tawk <theresiatawk@hotmail.com>
2023-05-08 16:10:10 +0300

    Sending login requests to the banner:

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import requests
 
 def getCred():
     username = input("Enter your username: ")
@@ -8,3 +8,11 @@ def getCred():
     credentials['password'] = password
 
     return credentials
+
+def sendingRequest(credentials):
+    response = requests.post("https://banweb.lau.edu.lb/pkmslogin.form" , data=credentials)
+    return response.text
+
+
+credentials = getCred()
+print(sendingRequest(credentials))

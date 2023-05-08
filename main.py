2f14c637654bfe417a4e014b2d2974fbe7edc3e6
Theresia Tawk <theresiatawk@hotmail.com>
2023-05-08 16:46:12 +0300

    Login to banner using selenium webdriver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from selenium import webdriver
+from selenium.webdriver.common.by import By 
 
 
 def getCred():
@@ -15,8 +16,18 @@ def sendingRequest(credentials):
     response = requests.post("https://banweb.lau.edu.lb/prod/pkmslogin.form" , data=credentials)
     return response.text
 
-driver = webdriver.Chrome()
-driver.get("https://banweb.lau.edu.lb/")
+# Creating new web driver to automatically fill the credentials
+webDriver = webdriver.Chrome()
+# Getting the banner login page into the driver
+webDriver.get("https://banweb.lau.edu.lb/")
+# 
+loginField = webDriver.find_element(by="id", value="username")
+loginField.send_keys("username")
+passwordField = webDriver.find_element(by="id", value="password")
+passwordField.send_keys("password")
+loginButton = webDriver.find_element(by=By.CSS_SELECTOR, value='[type="submit"]')
+loginButton.click()
+
 
 
 # credentials = getCred()
